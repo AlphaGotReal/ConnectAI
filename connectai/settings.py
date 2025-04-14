98820ee60ae14f12ddc6a93cd160858629f01589
Neil J George <neilgeorge1509@gmail.com>
2025-04-14 05:32:32 +0530

    Added redis for broadcasting messages

--- connectai/settings.py
+++ connectai/settings.py
@@ -64,8 +64,11 @@ WSGI_APPLICATION = 'connectai.wsgi.application'
 ASGI_APPLICATION = 'connectai.asgi.application'
 
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],  # Redis default port
+        },
     },
 }
 
